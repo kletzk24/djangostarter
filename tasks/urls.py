69d1c3b531bc2dc5bafb0488108f33e506e5ca17
KittenStorm26 <matthewkletzkin@gmail.com>
2026-02-10 21:46:34 -0500

    url made and view created

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ app_name="tasks"
 
 urlpatterns = [
     # path("compute/<int:value>", views.compute, name="compute"),
+    path("listtasks/<str:project>", views.listtasks, name="listtasks"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Task
+
 # Create your views here.
+def listtasks(request, project):
+    tasks = Task.objects.filter(project__name=project)
+
+    return render (request, "tasks.listtasks.html", {
+        'project': project,
+        'tasks': tasks
+    })
